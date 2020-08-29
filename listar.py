0d4c6bee7458529080745a2699adc637043555dd
palomara <palomarangelll@hotmail.com>
2020-08-29 12:25:26 -0300

    ajustes no listar

--- listar.py
+++ listar.py
@@ -5,12 +5,15 @@ def exibir_produto(codigo):
         if produto[0] == codigo:
             print("CÓDIGO\tDESCRIÇÃO\tQUANTIDADE EM ESTOQUE:")
             print(6 * '-', '', 9 * '-', ' ', 22 * '-')
-            print(f" {produto[0]}  {produto[1].ljust(20)} {produto[2]}")
+            print(f" {produto[0].ljust(6)}  {produto[1].ljust(20)} {produto[2]}")
             print(6 * '-', '', 9 * '-', ' ', 22 * '-')
 
 def listar_produtos():
+    qtd_cadastrados = 0
+    print("CÓDIGO\tDESCRIÇÃO\tQUANTIDADE EM ESTOQUE:")
     for produto in main.estoque:
-        print("CÓDIGO\tDESCRIÇÃO\tQUANTIDADE EM ESTOQUE:")
         print(6 * '-', '', 9 * '-', ' ', 22 * '-')
         print(f" {produto[0]}  {produto[1].ljust(20)} {produto[2]}")
-        print(6 * '-', '', 9 * '-', ' ', 22 * '-')
+        qtd_cadastrados += 1
+
+    print("\nTotal de produtos cadastrados: ", qtd_cadastrados)
