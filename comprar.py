53392c3f7aa7371849ad7ebb044e983e88790bdb
palomara <palomarangelll@hotmail.com>
2020-08-29 01:19:56 -0300

    comprar produtos

--- comprar.py
+++ comprar.py
@@ -1,24 +1,21 @@
 import main
+import listar
+
 
 def comprar_produto():
-    codigo = int(input("Código do produto: "))
-    for alterar in main.estoque:
-        if main.estoque[0] == codigo:
-            for produto in estoque:
-              print("CÓDIGO\tDESCRIÇÃO\tQUANTIDADE EM ESTOQUE:")
-              print(6 * '-', '', 9 * '-', ' ', 22 * '-')
-              print(f" {produto[0]}  {produto[1].ljust(20)} {produto[2]}")
-            quant_prod = int(input("Quanto deseja comprar: "))  # ==> Comprando somente o primeiro item da lista
-            while quant_prod <= 0:
-                print("\033[31mImpossível comprar uma quantia menor ou igual a zero\033[m")
-                quant_prod = int(input("Quanto deseja comprar: "))
+    codigo = input("Código do produto: ")
 
-            main.estoque[2] = quant_prod + main.estoque[2]
-            for produto in estoque:
-              print("CÓDIGO\tDESCRIÇÃO\tQUANTIDADE EM ESTOQUE:")
-              print(6 * '-', '', 9 * '-', ' ', 22 * '-')
-              print(f" {produto[0]}  {produto[1].ljust(20)} {produto[2]}")
-            break
-        else:
-            print("\033[31mProduto não cadastrado\033[m")
-            break
+    if codigo in main.codigo:
+        listar.exibir_produto(codigo)
+        qtd_prod = int(input("Quanto deseja comprar: "))
+        for produto in main.estoque:
+            if qtd_prod <= 0:
+                print("Quantia digitada é menor ou igual a zero.")
+            else:
+                if produto[0] == codigo:  # procura o código do produto dentro do estoque
+                    listar.exibir_produto(codigo)
+                    produto[2] = int(produto[2]) + qtd_prod  # realiza a soma da compra
+                    print("\t\tATUALIZAÇÃO: ")
+                    listar.exibir_produto(codigo)
+    else:
+        print("\033[31mProduto não cadastrado\033[m")
